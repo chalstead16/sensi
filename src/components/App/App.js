6c92afed2b068724fec76ccda43454055b73ec03
Casey Halstead <catherineclosee@gmail.com>
2022-04-20 16:27:32 -0500

    Add imports and useEffect to get quote data and functionality to add new entry

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import getQuotes from '../../apiCalls';
+import Form from '../Form/Form';
+import Entries from '../Entries/Entries';
 
 function App() {
+  const [quotes, setQuotes] = useState('');
+  const [error, setError] = useState('')
+  const [entries, setEntries] = useState([]);
+
+
+  useEffect(() => {
+    getQuotes()
+    .then(data => setQuotes(data))
+    .catch(error => {
+      setError('Hmm.. something went wrong')
+
+    })
+  }, [])
+
+  const addEntry = (newEntry) => {
+    console.log(entries, "this is entries")
+    setEntries([...entries, newEntry]);
+  }
+
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Form quotes={quotes} addEntry={addEntry}/>
+      {!entries.length && <h2>no previous entries yet</h2>}
+      <Entries entries={entries}/>
     </div>
   );
 }
