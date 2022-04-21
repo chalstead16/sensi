f79d7785e8baa7eca737dfbca3b3891fa98d0187
Casey Halstead <catherineclosee@gmail.com>
2022-04-21 14:24:49 -0500

    Add functionality to checkbox tasks

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './ToDoList.css';
+import Task from '../Task/Task';
 
-const ToDoList = ({todos}) => {
+const ToDoList = ({ todos, deleteTask }) => {
+
+  const allTodos = todos.map( todo => {
+    return (
+        <Task 
+          {...todo} 
+          key={todo.id}
+          deleteTask={deleteTask}
+          />
+    )
+  })
+
   return (
     <div className='todo-list'>
       <h2>todo list</h2>
-      {!todos.length && <h3>all tasks completed</h3>}
+      {!todos.length && <p>all tasks completed</p>}
+      {allTodos}
     </div>
   )
 }
