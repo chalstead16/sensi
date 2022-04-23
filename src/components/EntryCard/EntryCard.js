1f6c625f7c20daef574274dbc24d328505670a65
Casey Halstead <catherineclosee@gmail.com>
2022-04-22 19:30:01 -0500

    Add current emotion to entry card

--- src/components/EntryCard/EntryCard.js
+++ src/components/EntryCard/EntryCard.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
 import './EntryCard.css';
 
-const EntryCard = ({ id, quote, author, diaryEntry, like, handleLike}) => {
+const EntryCard = ({ id, quote, author, diaryEntry, emotion, like, handleLike}) => {
 
   return (
     <div className='card'>
       <div className='entry-text'>
         <p>{quote}</p>
         <p>{author}</p>
+        <p className='emotional-state'>emotion: <em>{emotion}</em></p>
         <p className="entry">{diaryEntry}</p>
       </div>  
       <div className='button-container'>
