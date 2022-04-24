adbd6df7e26f3d364a580e46cd2284cbe16796b7
Casey Halstead <catherineclosee@gmail.com>
2022-04-24 11:50:25 -0500

    Add error handling component

---
+++ src/components/Error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div className='error'>
+      <p>Hmm.. something went wrong, try again later.</p>
+    </div>
+  )
+}
+
+export default Error;
